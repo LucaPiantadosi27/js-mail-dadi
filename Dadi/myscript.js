32a2fd7bf0e994f423f107b128f4fc01a6b37ebf
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-05 18:19:49 +0100

    esercizio finito

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -1,6 +1,23 @@
 //Gioco dei dadi, chi fa di più vince
 
 
-//dichiarare variuabili
 
-console.log("ciao a tutti!");
+  // Lancio dado
+function myFunction() {
+  x = Math.floor((Math.random() * 6) + 1);
+  document.getElementById("first").innerHTML = x;
+  y = Math.floor((Math.random() * 6) + 1);
+  document.getElementById("second").innerHTML = y;
+
+  // Si verifica quale numero ha vinto o se c'è stato un pareggio
+  if (x > y) {
+    risultato = "L'USER HA VINTO ";
+  } else if (x == y) {
+      risultato = "I CONCORRENTI HANNO PAREGGIATO";
+    } else {
+      risultato = "IL COMPUTER HA VINTO";
+    }
+
+  // Si comunica chi ha vinto o se c'è stato un pareggio
+  document.getElementById('vincitore').innerHTML = "Risultato: " + risultato;
+}
